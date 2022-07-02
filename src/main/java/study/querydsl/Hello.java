f97beef69ce4b228ad450986b712178269a76827
JIWEON-JEONG <dlektl6@naver.com>
2022-07-02 19:02:48 +0900

    [ADD] 환경 구축

---
+++ src/main/java/study/querydsl/Hello.java
@@ -0,0 +1,19 @@
+package study.querydsl;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Hello {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+}
